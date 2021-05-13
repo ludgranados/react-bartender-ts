b6fe42736399d910c74bcd04b84714abafdca1b3
Ludwin Granados <ludwingranados@gmail.com>
2021-05-13 16:51:39 -0700

    Feature: Navbar edits

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark' style={{marginTop: '1%', opacity: '.7'}}>
       {/* Logo */}
-      <Link to='/' className='navbar-brand'>
-        Drinkify
+      <div style={{marginLeft: '1%'}}>
+        <Link to='/' className='navbar-brand'>
+        
+        <img src="https://i.ibb.co/BKm0F8M/drinkify.png" alt="Drinkify-Logo" height= '60' />
       </Link>
+      </div>
+      
 
       {/* Menu */}
-      <div className='collapse navbar-collapse'>
+      <div className='collapse navbar-collapse' style={{marginLeft: '3%', fontSize: '200%'}}>
         <div className='navbar-nav'>
           <Link className='nav-link' to='/'>
             Home
           </Link>
         </div>
+      </div>
+
+      <div style={{marginLeft: '1%'}}>
+        <Link to='/' className='navbar-brand'>
+        <img src="https://i.ibb.co/bgZvjsP/drinkify-tiki-bar.png" alt="Drinkify-round-tiki-bar-Logo" height= '70' />
+        </Link>
       </div>
       
     </nav>
